0496959f982cb02b682ea688193dd5f1bc4287e6
Sparsh <sparsh0987654321@gmail.com>
2021-12-30 00:08:03 +0530

    Updates

---
+++ precache-manifest.3af45c3713e0c68c1f249fa88b4c178e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9373a17b649a0315a3de9e8dbdc21b7",
+    "url": "/Tesla-Clone/index.html"
+  },
+  {
+    "revision": "401bdfa8e3125165b3b4",
+    "url": "/Tesla-Clone/static/css/main.3fcabf40.chunk.css"
+  },
+  {
+    "revision": "fad522da002a645b93b1",
+    "url": "/Tesla-Clone/static/js/2.21ee2c82.chunk.js"
+  },
+  {
+    "revision": "ec266338327c96f8d208bb8386b5b9a5",
+    "url": "/Tesla-Clone/static/js/2.21ee2c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "401bdfa8e3125165b3b4",
+    "url": "/Tesla-Clone/static/js/main.7bbbfaae.chunk.js"
+  },
+  {
+    "revision": "4a723991cd4cebae92db",
+    "url": "/Tesla-Clone/static/js/runtime-main.9b86b194.js"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "/Tesla-Clone/static/media/logo.be622899.svg"
+  }
+]);
